1c3d6bfc30a85dae2fb5a407c9943ca6970af241
Alejandro Berenguer Martín <alexberemart@gmail.com>
2020-04-19 10:55:54 +0200

    working

---
+++ basket-app-vue/src/components/HelloWorld.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>{{ product }}</h1>
+    <form>
+      <input v-model="message" placeholder="edit me">
+      <p>Message is: {{ message }}</p>
+      <button v-on:click="greet">Greet</button>
+    </form>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import myApi from './myApi'
+  import myBasketScrapingApi from './myBasketScrapingApi'
+
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        product: null,
+        message: null
+      }
+    },
+    mounted () {
+      this.getProducts()
+    },
+    methods: {
+      getProducts () {
+        myApi.get('getByReferenceId?referenceId=' + 'olynyke01').then(response => {
+          this.product = response.data
+        })
+      },
+      greet () {
+        let info = null
+        myBasketScrapingApi.get('basketreference/createJobFromYear/' + this.message).then(response => {
+          info = response.data
+        })
+        alert(info)
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
